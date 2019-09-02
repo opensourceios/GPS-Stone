cce45c9f179188bb0d35f4e7735b54666e166dcd
Frizlab <fload@me.com>
2019-09-02 08:38:13 +0200

    TMP

--- GPS Stone Trip Recorder/Views & Controllers/Main View/MapViewController.swift
+++ GPS Stone Trip Recorder/Views & Controllers/Main View/MapViewController.swift
@@ -85,6 +85,7 @@ class MapViewController : UIViewController, MKMapViewDelegate, NSFetchedResultsC
 			pointsFetchResultsController?.delegate = nil
 			pointsFetchResultsController = nil
 			
+			pointsProcessingQueue.cancelAllOperations()
 			mapView.removeOverlays(mapView.overlays)
 			polylinesCache = PolylinesCache()
 		}
@@ -128,10 +129,21 @@ fileprivate class PolylinesCache {
 	
 	var numberOfSections = 0
 	
+	/* The number of points currently added in the cache for a given section. */
 	var nPointsBySection = [Int]()
+	/* We break down each section to polylines of 100 points in order to avoid
+	 * having to redraw the whole path each time a new point is added. This is
+	 * why polylinesBySection is an array of array of polylines instead of a
+	 * simple array of polylines. */
 	var polylinesBySection = [[MKPolyline]]()
-	var interSectionPolylines = [MKPolyline]()
+	/* Between each sections we show a dotted line indicating missing information
+	 * (the recording was paused). This variable contains these polylines. They
+	 * optional because some section might not have any points in them, in which
+	 * case there are no dotted line to show, but we must still have an element
+	 * in the array to have the correct count of objects in the array. */
+	var interSectionPolylines = [MKPolyline?]()
 	
+	var polylinesToRemoveFromMap = [MKPolyline]()
 	var plainPolylinesToAddToMap = [MKPolyline]()
 	var dottedPolylinesToAddToMap = [MKPolyline]()
 	
@@ -172,7 +184,33 @@ fileprivate class ProcessPointsOperation : RetryingOperation {
 	}
 	
 	override func startBaseOperation(isRetry: Bool) {
-		print(pointsToProcess)
+		let startSectionIndex = max(polylinesCache.numberOfSections-1, 0)
+		
+		for (sectionDelta, pointsInSection) in pointsToProcess.enumerated() {
+			let sectionIndex = startSectionIndex + sectionDelta
+			assert(sectionIndex <= polylinesCache.numberOfSections)
+			
+			/* Add a section in the cache if needed */
+			if sectionIndex == polylinesCache.numberOfSections {
+				polylinesCache.numberOfSections += 1
+				polylinesCache.nPointsBySection.append(0)
+				polylinesCache.polylinesBySection.append([])
+				if sectionDelta+1 < pointsInSection.count {
+					if let p1 = pointsInSection.first, let p2 = pointsToProcess[sectionDelta+1].first {
+						let l = MKPolyline(coordinates: [p1, p2], count: 2)
+						polylinesCache.interSectionPolylines.append(l)
+						polylinesCache.dottedPolylinesToAddToMap.append(l)
+					} else {
+						polylinesCache.interSectionPolylines.append(nil)
+					}
+				}
+			}
+			
+			/* Add the new points in the current section */
+			let latestPolylineOfSection = polylinesCache.polylinesBySection[sectionIndex].last
+			
+		}
+		
 		baseOperationEnded()
 	}
 	
